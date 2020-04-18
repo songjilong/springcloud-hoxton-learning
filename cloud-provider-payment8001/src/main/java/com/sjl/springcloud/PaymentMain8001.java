0d7ddd63b2e78cecd37f36f5a18046a7233c3583
songjilong <2606587750@qq.com>
2020-04-18 20:16:50 +0800

    初始化项目结构

---
+++ cloud-provider-payment8001/src/main/java/com/sjl/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.sjl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songjilong
+ * @date 2020/4/18 19:53
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class);
+    }
+}
