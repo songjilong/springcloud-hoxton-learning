e031d6a50cca771e697665d6c3e480ceb5e6c3fa
songjilong <2606587750@qq.com>
2020-04-26 20:51:16 +0800

    Spring Cloud Bus

---
+++ cloud-config-client3366/src/main/java/com/sjl/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,15 @@
+package com.sjl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songjilong
+ * @date 2020/4/26 14:16
+ */
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
